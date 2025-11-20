d08e2fe9b7d2a94d0af2ac0cf5c27dd61b850df7
Adrien Legeleux <adrienlegeleu@gmail.com>
2025-11-20 01:38:00 +0100

    nassim

--- components/FinalCTA.tsx
+++ components/FinalCTA.tsx
@@ -11,7 +11,7 @@ export default function FinalCTA() {
     >
       <div className="mx-auto max-w-[1200px] relative space-y-10">
         <h2 className="text-3xl md:text-4xl font-bold text-center">
-          Réserve ton appel en 30 secondes
+          Réserve ton rendez-vous en 30 secondes
         </h2>
 
         <CalendlyInlineWidget url="https://calendly.com/contact-nettoyageriviera/30min" />
--- components/pro/ProCTA.tsx
+++ components/pro/ProCTA.tsx
@@ -8,7 +8,7 @@ export default function ProCTA() {
     <section className="relative bg-white text-neutral-900 py-24 md:py-32 px-5">
       <div className="mx-auto max-w-[1200px] relative space-y-10">
         <h2 className="text-3xl md:text-4xl font-bold text-center">
-          Réserve ton appel en 30 secondes
+          Réserve ton rendez-vous en 30 secondes
         </h2>
 
         <CalendlyInlineWidget url="https://calendly.com/contact-nettoyageriviera/30min" />
